d66c9d3a4cb5af9b3efc63d6dc24b057f13a9147
Shrunal Nimje <shrunalnimje@gmail.com>
2025-07-19 12:48:25 +0530

    Initial commit

---
+++ src/main/java/com/mymood/smartcomplaint/SmartComplaintSystem/SmartComplaintSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mymood.smartcomplaint.SmartComplaintSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartComplaintSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartComplaintSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymood/smartcomplaint/SmartComplaintSystem/SmartComplaintSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymood.smartcomplaint.SmartComplaintSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartComplaintSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
